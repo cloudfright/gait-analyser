3e65e0968843a2115e5b57b0a25182b2fed719da
Ben Dyer <ben.dyer@hey.com>
2022-07-29 10:54:20 +0100

    Test audio with permission workaround

--- scripts/sensors.js
+++ scripts/sensors.js
@@ -3,13 +3,23 @@ const AccelerationThreshold = 1;
 
 let sound;
 
-function initialiseSound() {
+function initSound() {
 
+  // we don't need the mic, but it seems to be the only way to get the audio context unmuted when asking for permission
+  var mic = new Pizzicato.Sound({
+    source: 'input',
+    options: { volume: 0 }
+  });
 
   sound = new Pizzicato.Sound({
     source: 'wave',
     options: { type: 'square', frequency: 440, attack: 0 }
   });
+  
+  var group = new Pizzicato.Group();
+
+  group.addSound(mic);
+  group.addSound(sound);
   
   var stereoPanner = new Pizzicato.Effects.StereoPanner({
     pan: 0.5
@@ -18,10 +28,8 @@ function initialiseSound() {
   sound.addEffect(stereoPanner);
 }
 
-function requestPermission() {
+function requestMotionPermission() {
 
-  initialiseSound(); 
-  
   if (typeof DeviceMotionEvent.requestPermission === 'function') {
     // Handle iOS 13+ devices.
     DeviceMotionEvent.requestPermission()
@@ -73,6 +81,27 @@ function handleMotion(event) {
   event.accelerationIncludingGravity.x - event.acceleration.x
   event.accelerationIncludingGravity.y - event.acceleration.y
   event.accelerationIncludingGravity.z - event.acceleration.z
+https://stackoverflow.com/questions/57942230/ios-safari-web-audio-api-restriction-problem
+https://developer.apple.com/library/archive/documentation/AudioVideo/Conceptual/Using_HTML5_Audio_Video/PlayingandSynthesizingSounds/PlayingandSynthesizingSounds.html
+https://github.com/CreateJS/SoundJS
+
+https://paulbakaus.com/tutorials/html5/web-audio-on-ios/
+
+https://stackoverflow.com/questions/12517000/no-sound-on-ios-6-web-audio-api
+
+https://gist.github.com/kus/3f01d60569eeadefe3a1
+
+just resume like this:
+
+document.addEventListener('touchend', ()=>window.audioContext.resume());
+
+https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Best_practices
+
+https://developer.chrome.com/blog/autoplay/#webaudio
+
+---> https://github.com/alemangui/pizzicato/issues/115
+
+
 */
 
 }
